e9215f3a39ed589bb256126009020d679e41fe19
madhukar01 <mholla8@gmail.com>
2022-12-03 19:34:45 +0530

    feat(players): player generation

--- app/import_routes.py
+++ app/import_routes.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+
 from rest_server.monitoring import heartbeat
 from rest_server.monitoring import index
+from rest_server.team import get_players
 from rest_server.user import create_user
 from rest_server.user import get_user
 
@@ -20,3 +22,8 @@ def import_routes(app: FastAPI) -> None:
     ###########################################################################
     app.include_router(get_user.router)
     app.include_router(create_user.router)
+
+    ###########################################################################
+    # Team
+    ###########################################################################
+    app.include_router(get_players.router)
