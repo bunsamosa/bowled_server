a02e6251ae0f1ded7011e3f4336a076a65652706
madhukar01 <mholla8@gmail.com>
2023-01-28 12:46:13 -0500

    feat(game): live game without auth

--- app/import_routes.py
+++ app/import_routes.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from rest_server.live import get_teams
+from rest_server.live import live_game
 from rest_server.monitoring import heartbeat
 from rest_server.monitoring import index
 from rest_server.team import get_players
@@ -35,3 +36,4 @@ def import_routes(app: FastAPI) -> None:
     # Live
     ###########################################################################
     app.include_router(get_teams.router)
+    app.include_router(live_game.router)
