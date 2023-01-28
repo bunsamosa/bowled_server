4e4656d7f85a5f8d8b6d439099a7e01c065db6fd
madhukar01 <mholla8@gmail.com>
2023-01-28 11:17:03 -0500

    fix(live): fix team value rendering

--- rest_server/live/get_teams.py
+++ rest_server/live/get_teams.py
@@ -10,7 +10,7 @@ from fastapi import Request
 router = APIRouter(prefix="/live")
 
 
-@router.get(path="/teams", response_model=List, tags=["Team"])
+@router.get(path="/teams", response_model=List, tags=["Live"])
 async def get_teams(request: Request) -> Union[List, HTTPException]:
     """
     Get teams API
@@ -22,26 +22,26 @@ async def get_teams(request: Request) -> Union[List, HTTPException]:
     # Read available teams data from redis
     # teams = cache_store.get_dictionary("live_teams")
     # TODO: read data from database
-    teams = [
-        {
+    teams = {
+        "mi": {
             "team_name": "Mumbai Indians",
             "team_id": "mi",
             "team_logo": "https://static.wikia.nocookie.net/logopedia/images/5/50/MI.png",
         },
-        {
+        "kkr": {
             "team_name": "Kolkata Knight Riders",
             "team_id": "kkr",
             "team_logo": "https://static.wikia.nocookie.net/logopedia/images/4/49/Kolkata_Knight_Riders_logo.svg/",
         },
-        {
+        "csk": {
             "team_name": "Chennai Super Kings",
             "team_id": "csk",
             "team_logo": "https://static.wikia.nocookie.net/logopedia/images/8/83/Chennai_Super_Kings_logo.svg",
         },
-        {
+        "dc": {
             "team_name": "Delhi Capitals",
             "team_id": "dc",
             "team_logo": "https://static.wikia.nocookie.net/logopedia/images/d/d2/Delhi_Capitals.png",
         },
-    ]
+    }
     return tuple(teams.values())
