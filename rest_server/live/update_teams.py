770a004e90529b4fa928d86fe18cbea40689adfc
madhukar01 <mholla8@gmail.com>
2023-02-13 19:11:57 -0500

    fix(team): clear existing data when updating team

--- rest_server/live/update_teams.py
+++ rest_server/live/update_teams.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from fastapi import HTTPException
 from fastapi import Request
 
+from lib.utils.player_generator import fill_skill_colors
 
 # Create FastAPI router
 router = APIRouter(prefix="/live")
@@ -24,13 +25,19 @@ async def update_teams(request: Request, data: Dict) -> HTTPException:
 
     id_gen = cache_store.get_id_generator(key="players")
     for team_id in data:
+        player_data = []
         players = data[team_id]["players"]
 
         for player in players:
             player_id = next(id_gen)
             player["player_id"] = player_id
+            updated_player = fill_skill_colors(player)
+            player_data.append(updated_player)
+
+        data[team_id]["players"] = player_data
 
     # Read available teams data from redis
+    live_teams.clear()
     live_teams.update(data)
 
     return live_teams
