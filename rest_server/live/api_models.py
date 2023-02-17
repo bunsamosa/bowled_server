6402f587b0e3268abe7c5824a26f24251cab8f87
Dhanush Kanchan <dhanushkanchan@gmail.com>
2023-02-17 16:24:49 -0500

    fix(players): fetch player data using gamelib

--- rest_server/live/api_models.py
+++ rest_server/live/api_models.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from pydantic import BaseModel
+from pydantic import Field
 
 
 class PublicTeam(BaseModel):
@@ -8,9 +9,50 @@ class PublicTeam(BaseModel):
     This model represents a public team
     """
 
-    team_id: str
-    team_logo: str
-    team_name: str
+    team_id: str = Field(description="Team ID")
+    team_logo: str = Field(description="URL to team logo image")
+    team_name: str = Field(description="Team name")
+
+
+class PublicTeamPlayer(BaseModel):
+    """
+    This model represents a public team player
+    """
+
+    player_id: int = Field(description="Player ID")
+    team_id: str = Field(description="Team ID")
+    player_name: str = Field(description="Player name")
+    player_type: str = Field(description="Player type")
+    dob: str = Field(description="Date of birth in milliseconds")
+    fitness: int = Field(description="Fitness attribute level")
+    form: int = Field(description="Form attribute level")
+    batting_type: str = Field(description="Batting type")
+    bowling_type: str = Field(description="Bowling type")
+    fielding: int = Field(description="Fielding skill level")
+    fielding_color: str = Field(description="Fielding skill color")
+    wicket_keeping: int = Field(description="Wicket keeping skill level")
+    wicket_keeping_color: str = Field(description="Wicket keeping skill color")
+    batting_seam: int = Field(description="Batting seam skill level")
+    batting_seam_color: str = Field(description="Batting seam skill color")
+    batting_spin: int = Field(description="Batting spin skill level")
+    batting_spin_color: str = Field(description="Batting spin skill color")
+    bowling_main: int = Field(description="Bowling main skill level")
+    bowling_main_color: str = Field(description="Bowling main skill color")
+    bowling_variation: int = Field(description="Bowling variation skill level")
+    bowling_variation_color: str = Field(
+        description="Bowling variation skill color",
+    )
+    batting_rating: int = Field(description="Batting rating")
+    bowling_rating: int = Field(description="Bowling rating")
+
+
+class LiveMetrics(BaseModel):
+    """
+    This model represents live metrics such as games live and total games
+    """
+
+    games_played: int = Field(default=0, description="Total games played")
+    games_live: int = Field(default=0, description="Games live now")
 
 
 class LiveGameInput(BaseModel):
--- rest_server/live/live_players.py
+++ rest_server/live/live_players.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter
 from fastapi import HTTPException
 from fastapi import Request
 
-from lib.utils.player_generator import fill_skill_colors
+from lib.game.team.public_team import get_players_by_team_id
 
 # Create FastAPI router
 router = APIRouter(prefix="/live")
@@ -14,23 +14,22 @@ router = APIRouter(prefix="/live")
 @router.get(path="/players", response_model=List, tags=["Live"])
 async def get_players(
     request: Request,
-    myteam: str,
+    team: str,
 ) -> Union[List, HTTPException]:
     """
-    Get players API
+    This API returns players for a given team
     """
-    cache_store = request.app.cache_store
+    datastore = request.app.data_store
+    cachestore = request.app.cache_store
     logger = request.app.logger
     await logger.info("Get players API")
-    live_teams = cache_store.get_dictionary(key="live_teams")
-    team_data = live_teams.get(myteam)
 
-    # Update player data
-    player_data = []
-    for player in team_data["players"]:
-        updated_player = fill_skill_colors(player)
-        player_data.append(updated_player)
-    team_data["players"] = player_data
-    live_teams[myteam] = team_data
+    # Fetch players data from postgres
+    async with datastore.acquire() as connection:
+        player_data = await get_players_by_team_id(
+            team_id=team,
+            ds_connection=connection,
+            cachestore=cachestore,
+        )
 
     return player_data
