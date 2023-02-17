222bd1e3dcdc8a6e20dbd91c998cc67fd69102ba
madhukar01 <mholla8@gmail.com>
2023-02-17 16:24:49 -0500

    fix(players): fetch player data using gamelib

--- rest_server/live/get_metrics.py
+++ rest_server/live/get_metrics.py
@@ -1,26 +1,25 @@
-from typing import Dict
-from typing import Union
-
 from fastapi import APIRouter
-from fastapi import HTTPException
 from fastapi import Request
+
+from rest_server.live.api_models import LiveMetrics
 
 
 # Create FastAPI router
 router = APIRouter(prefix="/live")
 
 
-@router.get(path="/metrics", response_model=Dict, tags=["Live"])
+@router.get(path="/metrics", response_model=LiveMetrics, tags=["Live"])
 async def get_live_metrics(
     request: Request,
     resetlive: bool = False,
-) -> Union[dict, HTTPException]:
+) -> LiveMetrics:
     """
-    Get metrics API
+    This API returns live metrics such as games live and total games
+    :param resetlive: Reset current live games
     """
     cache_store = request.app.cache_store
     logger = request.app.logger
-    await logger.info("Live metrics API")
+    await logger.info("Get live metrics API")
 
     # Read available teams data from redis
     live_metrics = cache_store.get_dictionary("live_metrics")
