06dd14e71e06dde908c18e75450cd452ba47d7a7
madhukar01 <mholla8@gmail.com>
2023-02-17 11:48:50 -0500

    fix(teams): fetch team data from DB

--- rest_server/live/api_models.py
+++ rest_server/live/api_models.py
@@ -3,6 +3,16 @@ from typing import List
 from pydantic import BaseModel
 
 
+class PublicTeam(BaseModel):
+    """
+    This model represents a public team
+    """
+
+    team_id: str
+    team_logo: str
+    team_name: str
+
+
 class LiveGameInput(BaseModel):
     team_id: str
     batting_lineup: List[int]
