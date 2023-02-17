6bfe753732ef7a1f4a73a5ddab371a6fdb2f6dd0
madhukar01 <mholla8@gmail.com>
2023-02-17 16:25:38 -0500

    feat(cache): system cache manager with reload api

---
+++ lib/cache_manager/load_skills_attributes.py
@@ -0,0 +1,35 @@
+from pypika.terms import Star
+
+from lib.game.data_models import attribute_index_labels
+from lib.game.data_models import skill_index_labels
+
+
+async def load_data(ds_connection, cachestore) -> None:
+    """
+    This function loads skill and attribute labels into cachestore
+    :param ds_connection: datastore connection
+    :param cachestore: cachestore connection
+    """
+    # fetch skill labels from postgres
+    skill_query = skill_index_labels.select(Star())
+    skill_query = skill_query.get_sql()
+    skill_data = await ds_connection.fetch(skill_query)
+
+    # store processed data in cachestore
+    skill_labels = cachestore.get_dictionary("skill_labels")
+    skill_labels.clear()
+    for skill in skill_data:
+        skill_index = skill["skill_index"]
+        skill_labels[skill_index] = dict(skill)
+
+    # fetch attribute labels from postgres
+    attribute_query = attribute_index_labels.select(Star())
+    attribute_query = attribute_query.get_sql()
+    attribute_data = await ds_connection.fetch(attribute_query)
+
+    # store processed data in cachestore
+    attribute_labels = cachestore.get_dictionary("attribute_labels")
+    attribute_labels.clear()
+    for attribute in attribute_data:
+        attribute_index = attribute["attribute_index"]
+        attribute_labels[attribute_index] = dict(attribute)
---
+++ rest_server/system_management/reload_cache.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import Response
+
+from lib.cache_manager import load_skills_attributes
+
+
+# Create FastAPI router
+router = APIRouter(prefix="/system")
+
+
+@router.get(path="/reload-cache", tags=["System"])
+async def reload_cache(request: Request):
+    """
+    This API calls the cache manager to reload the cache
+    """
+    logger = request.app.logger
+    datastore = request.app.data_store
+    cachestore = request.app.cache_store
+    await logger.info("Reload cache API")
+
+    # Reload cache
+    async with datastore.acquire() as connection:
+        await load_skills_attributes.load_data(
+            ds_connection=connection,
+            cachestore=cachestore,
+        )
+    await logger.info("Cache reloaded successfully")
+
+    return Response(status_code=200)
